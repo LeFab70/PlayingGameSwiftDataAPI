d34f0faa5614fe2faff31c146d56fa354235c22a
LeFab70 <kouonang2002@gmail.com>
2025-08-05 06:36:42 -0300

    Initial Commit

---
+++ PlayingGameSwiftDataAPI/PlayingGameSwiftDataAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayingGameSwiftDataAPIApp.swift
+//  PlayingGameSwiftDataAPI
+//
+//  Created by Fabrice Kouonang on 2025-08-05.
+//
+
+import SwiftUI
+
+@main
+struct PlayingGameSwiftDataAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
